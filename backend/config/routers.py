3abd60f9d433475523fbcebefc49accaa1bbe4c7
Jesús Perales <22j.perales@gmail.com>
2025-12-21 15:27:12 -0400

    feat(config): add MasterRouter
    - Creates routers.py with MasterRouter
    - Registers all app view-sets (categories, tags, posts, uploads,
    users, profiles)

---
+++ backend/config/routers.py
@@ -0,0 +1,23 @@
+from apps.content.views import CategoryViewSet, PostViewSet, TagViewSet
+from apps.uploads.views import UploadViewSet
+from apps.users.views import ProfileViewSet, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+
+class MasterRouter(DefaultRouter):
+    def get_api_root_view(self, api_urls=None):
+        root_view = super().get_api_root_view(api_urls=api_urls)
+        root_view.cls.__doc__ = "Welcome to the API Browser."
+        root_view.cls.get_view_name = lambda self: "API Browser"
+        return root_view
+
+
+router = MasterRouter()
+
+router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"tags", TagViewSet, basename="tag")
+router.register(r"posts", PostViewSet, basename="post")
+router.register(r"uploads", UploadViewSet, basename="upload")
+router.register(r"users", UserViewSet, basename="user")
+router.register(r"profiles", ProfileViewSet, basename="profile")
+# router.register(r'<resource_name>', <viewset>, basename='<instance_resource_name>')
