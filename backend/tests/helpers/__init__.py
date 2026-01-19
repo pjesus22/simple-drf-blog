01138ce880d1b4e642be41b6703c86d5308304d0
Jesús Perales <22j.perales@gmail.com>
2026-01-19 18:42:00 -0400

    style(tests): update imports after module rename

--- backend/tests/helpers/__init__.py
+++ backend/tests/helpers/__init__.py
@@ -1,9 +1,9 @@
-from .datetime import assert_datetimes_close
-from .jsonapi import (
+from .api import (
     assert_drf_error_response,
     assert_jsonapi_error_pointers,
     assert_jsonapi_error_response,
 )
+from .datetime import assert_datetimes_close
 
 __all__ = [
     "assert_jsonapi_error_response",
