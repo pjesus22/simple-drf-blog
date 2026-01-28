593543b75027d9d62e2901f2dc154fa0920cc1c8
Jesús Perales <22j.perales@gmail.com>
2026-01-27 21:21:18 -0400

    chore(tests): register default user factory

--- backend/apps/accounts/tests/conftest.py
+++ backend/apps/accounts/tests/conftest.py
@@ -1,11 +1,13 @@
 from pytest_factoryboy import register
 from tests.factories import (
     AdminFactory,
+    DefaultUserFactory,
     EditorFactory,
     ProfileFactory,
     SocialLinkFactory,
 )
 
+register(DefaultUserFactory)
 register(EditorFactory)
 register(ProfileFactory)
 register(AdminFactory)
