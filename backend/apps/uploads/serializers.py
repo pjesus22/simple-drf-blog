9a0840ebb951c7a6ea25b42df2e11af66d78c261
Jesús Perales <22j.perales@gmail.com>
2026-01-30 23:21:22 -0400

    feat(uploads): split serializer
    - Introduce `UploadCreateSerializer` with required file field
    - Make `file` optional in base serializer
    - Update visibility check to use `User.Role.ADMIN` instead of
    `is_staff`
    - Refactor field order and serialization mappings for consistency

--- backend/apps/uploads/serializers.py
+++ backend/apps/uploads/serializers.py
@@ -1,23 +1,27 @@
+from django.contrib.auth import get_user_model
 from rest_framework_json_api import serializers
 
 from .models import Upload
+
+User = get_user_model()
 
 
 class UploadSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField(read_only=True)
     uploaded_by = serializers.ResourceRelatedField(read_only=True)
-    file = serializers.FileField(write_only=True, required=True)
+    file = serializers.FileField(write_only=True, required=False)
 
     included_serializers = {
-        "uploaded_by": "apps.accounts.serializers.UserListSerializer"
+        "uploaded_by": "apps.accounts.serializers.users.BaseUserSerializer",
     }
 
     class Meta:
         model = Upload
         fields = (
             "id",
-            "url",
+            "uploaded_by",
             "file",
+            "url",
             "original_filename",
             "mime_type",
             "size",
@@ -27,10 +31,10 @@ class UploadSerializer(serializers.ModelSerializer):
             "visibility",
             "created_at",
             "updated_at",
-            "uploaded_by",
         )
         read_only_fields = (
             "id",
+            "uploaded_by",
             "url",
             "original_filename",
             "mime_type",
@@ -39,7 +43,6 @@ class UploadSerializer(serializers.ModelSerializer):
             "height",
             "created_at",
             "updated_at",
-            "uploaded_by",
         )
         resource_name = "uploads"
 
@@ -49,10 +52,16 @@ class UploadSerializer(serializers.ModelSerializer):
         if obj.visibility == Upload.Visibility.PRIVATE:
             if not request or not request.user.is_authenticated:
                 return
-            if obj.uploaded_by != request.user and not request.user.is_staff:
+            if (
+                obj.uploaded_by != request.user
+                and not request.user.role == User.Role.ADMIN
+            ):
                 return
 
         if request:
             return request.build_absolute_uri(obj.file.url)
-
         return obj.file.url
+
+
+class UploadCreateSerializer(UploadSerializer):
+    file = serializers.FileField(write_only=True, required=True)
