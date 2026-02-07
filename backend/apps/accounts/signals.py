6f6ac6b5e1b81326a19a7194f6be5a9183730ec0
Jesús Perales <22j.perales@gmail.com>
2026-02-07 19:32:32 -0400

    refactor(accounts): update signal module
    Update the `post_save` signal to reference the renamed `Profile` model
    instead of `EditorProfile`, and rename the signal handler function from
    `create_editor_profile` to `create_profile` to maintain consistency with
    the model renaming.

--- backend/apps/accounts/signals.py
+++ backend/apps/accounts/signals.py
@@ -1,10 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import EditorProfile, User
+from .models import Profile, User
 
 
 @receiver(signal=post_save, sender=User)
-def create_editor_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     if created and instance.role == User.Role.EDITOR:
-        EditorProfile.objects.get_or_create(user=instance)
+        Profile.objects.get_or_create(user=instance)
