c43db35487c0d1fa65caef37cbb5e8f3369d2433
Jesús Perales <22j.perales@gmail.com>
2026-02-11 18:30:58 -0400

    chore(scripts): remove key generation scripts

--- scripts/api_keygen.py
+++
@@ -1,4 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-
-SECRET_KEY = get_random_secret_key()
-print(f"SECRET_KEY={SECRET_KEY}")
--- scripts/encryption_keygen.py
+++
@@ -1,4 +0,0 @@
-from cryptography.fernet import Fernet
-
-SECRET_ENCRYPTION_KEY = Fernet.generate_key()
-print(f"SECRET_ENCRYPTION_KEY={SECRET_ENCRYPTION_KEY.decode()}")
